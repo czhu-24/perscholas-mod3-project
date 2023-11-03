3d919f5c65c7c4ab9e97076607a18fb3e70ee3f2
czhu-24 <blue.ink.stains@gmail.com>
2023-11-03 02:48:39 -0700

    ah

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
 
-const { Schema } = mongoose; 
+const { Schema } = mongoose;
 
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true, unique: true},
-    hashedPassword: {type: String, required: true}
+    username: { type: String, required: true, unique: true },
+    // just remember that this pw is hashed!
+    password: { type: String, required: true },
+    profileDescription: { type: String },
+    profilePhoto: { type: String }
 },
-{
-    timestamps: true
-})
+    {
+        timestamps: true
+    })
 //  points to users collection
 const User = mongoose.model('User', userSchema)
 module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,28 @@ const morgan = require('morgan');
 const helmet = require('helmet'); // adds a bunch of standard security to server
 require('./config/db.js');
 const path = require('path');
-const User = require('./models/User.js');
-const Post = require('./models/Post.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const PORT = 3000;
 const app = express();
+
+const User = require('./models/User.js');
+const Post = require('./models/Post.js');
 
 // START MIDDLEWARE //
 app.use(express.json());
 app.use(cors({
-    origin: "*"
+  origin: "*"
 }));
 app.use(morgan('dev'));
 app.use(helmet());
 
-app.use((req, res, next)=> {
-    if (req.path.startsWith('/server')) {
-        req.url = req.url.replace('/server', ''); // strip /server from the path
-    }
-    next();
+app.use((req, res, next) => {
+  if (req.path.startsWith('/server')) {
+    req.url = req.url.replace('/server', ''); // strip /server from the path
+  }
+  next();
 })
 
 // END MIDDLEWARE //
@@ -31,12 +35,69 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 
 //                CREATE 
 
+
+
+app.post('/signup', async (req, res) => {
+  try {
+    const newUser = req.body;
+    // Hashing takes time
+    const hashedPassword = await bcrypt.hash(newUser.password, 10);
+
+    //console.log(newUser);
+
+    const dbUser = new User({
+      username: newUser.username,
+      password: hashedPassword,
+    });
+
+    await dbUser.save(); // Use await to handle the promise that's created by .save()
+    console.log("user added")
+    res.status(201).send({ message: "User added!" });
+  } catch (err) {
+    if (err.code === 11000) {
+      console.log("error duplicate username")
+      res.status(400).send({ message: "Error: duplicate username" });
+
+    } else {
+      console.log("sometign else")
+      res.status(400).send({ message: `Other error:  + ${err.message}` };
+    }
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const newUser = req.body;
+  // 1. get the user with this username
+  const dbUser = await User.findOne({ username: newUser.username });
+  // compare
+  // 2. see if there is a user by the provided username
+  if (!dbUser) {
+    return res.status(400).send("username or password incorrect")
+  };
+  // compare hashed version of front end entered pw & db's hashed pw
+  bcrypt.compare(newUser.password, dbUser.password, (err, isMatch) => {
+    if (isMatch) {
+      // let the frontend know that the login was successful!
+      // don't want hashed password to be passed to front end
+      dbUser.password = "";
+      // now just username
+      const token = jwt.sign({ dbUser }, process.env.TOKEN_SECRET, { expiresIn: "3h" });
+      res.status(200).send({ token, dbUser });
+
+      // log them in ( on frontend can do certain things, get info related to account, can do BACKEND stuff related to their account, permissions for CRUD functionality related to their account, allow only certain users to do certain things )
+    } else {
+      res.status(400).send("username or password incorrect")
+    }
+  })
+});
+
+
 app.post('/posts/create', async (req, res) => {
-  try{
+  try {
     const newPost = req.body;
     const dbResponse = await Post.create(newPost);
     res.status(201).send(dbResponse);
-  }catch(err){
+  } catch (err) {
     res.status(500).send("Error adding post to db", err);
   }
 })
@@ -45,7 +106,7 @@ app.post('/posts/create', async (req, res) => {
 //                READ
 
 app.get('/posts/read', async (req, res) => {
-  try{
+  try {
     const dbResponse = await Post.find();
     const changedDbResponse = dbResponse.map((post) => ({
       _id: post._id,
@@ -58,20 +119,20 @@ app.get('/posts/read', async (req, res) => {
     }));
 
     res.status(200).send(changedDbResponse);
-  }catch(err){
+  } catch (err) {
     res.status(500).send("Error getting all posts", err);
   }
 })
 
 //                UPDATE
 app.put('/posts/edit/:postId', async (req, res) => {
-  try{
+  try {
     const postId = req.params.postId;
     const newPost = req.body;
     //console.log(postId, newPost);
-    const dbResponse = await Post.findByIdAndUpdate(postId, newPost, {new: true});
+    const dbResponse = await Post.findByIdAndUpdate(postId, newPost, { new: true });
     res.status(200).send(dbResponse);
-  }catch(err){
+  } catch (err) {
     res.status(400).send("Error editing post", err);
   }
 })
@@ -79,11 +140,11 @@ app.put('/posts/edit/:postId', async (req, res) => {
 //                DELETE
 
 app.delete('/posts/delete/:postId', async (req, res) => {
-  try{
+  try {
     const postId = req.params.postId;
     const dbResponse = await Post.findByIdAndDelete(postId);
     res.status(200).send(dbResponse);
-  }catch(err){
+  } catch (err) {
     res.status(500).send("Error deleting post", err);
   }
 })
@@ -97,5 +158,5 @@ app.get('/*', (req, res) => {
 // END ROUTES //
 
 app.listen(PORT, () => {
-    console.log(`Server LIVE on port ${PORT}`);
+  console.log(`Server LIVE on port ${PORT}`);
 });
